37f2493781bf070bf1f3ef46c091449dc5a78d5f
AjdiNNN <ajdinhukic007@gmail.com>
2022-12-05 21:10:30 +0100

    Board dimension logic

--- main.go
+++ main.go
@@ -2,8 +2,67 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 )
 
+func getNewNumber(minX int, minY int) (int, int) {
+	var new = ""
+	//var err error
+	//var newNo int
+	re := regexp.MustCompile(`(\d+)x(\d+)`)
+	fmt.Println("Type board dimensions rows must not be less than " + strconv.FormatInt(int64(minX), 10) + " and columns muss not be less than " + strconv.FormatInt(int64(minY), 10))
+	fmt.Println("Type in format (rows)x(columns) ex. 6x7 and values of rows and columns must not differ by more than 2")
+	for true {
+		fmt.Scanln(&new)
+		if re.MatchString(new) {
+			value := re.FindAllStringSubmatch(new, 2)[0]
+			x, error1 := strconv.Atoi(value[1])
+			if error1 != nil {
+				fmt.Println("Error during conversion")
+				return 6, 7
+			}
+			y, error2 := strconv.Atoi(value[2])
+			if error2 != nil {
+				fmt.Println("Error during conversion")
+				return 6, 7
+			}
+			if x >= 6 {
+				if y >= 7 {
+					if math.Abs(float64(x-y)) < 2 {
+						return x, y
+					} else {
+						fmt.Println("Rows and columns must not differ by more than 2")
+						continue
+					}
+				} else {
+					fmt.Println(value[2] + " is not greater than or equal " + strconv.FormatInt(int64(minY), 10))
+					continue
+				}
+			} else {
+				fmt.Println(value[1] + " is not greater than or equal " + strconv.FormatInt(int64(minX), 10))
+				continue
+			}
+		} else {
+			fmt.Println(new + " is invalid format please try with (rows)x(columns)")
+		}
+	}
+	fmt.Printf("String contains any match: %v\n", re.FindAllStringSubmatch(new, 2)) // True
+	return 0, 0
+}
 func main() {
-	fmt.Println("Hello World!")
+	var xDefault, yDefault int = 6, 7
+	var x, y int = xDefault, yDefault
+	fmt.Println("Do you want to change board dimensions (6x7 is default): (y/n)")
+	var boardAnswer = ""
+	fmt.Scanln(&boardAnswer)
+	for boardAnswer != "y" && boardAnswer != "n" {
+		fmt.Println(boardAnswer + " is not valid (y/n)")
+		fmt.Scanln(&boardAnswer)
+	}
+	if boardAnswer == "y" {
+		x, y = getNewNumber(xDefault, yDefault)
+	}
+	fmt.Println(x + y)
 }
